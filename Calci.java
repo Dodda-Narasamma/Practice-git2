113a1e4d6048c866d30b411d4d6c2038b6e2154c
Dodda-Narasamma <doddamandadinarasamma3@gmail.com>
2024-02-29 12:30:46 +0530

    main4 class given by dev1

--- Calci.java
+++ Calci.java
@@ -1,3 +1,4 @@
+import java.util.*;
 interface Calci{
 public abstract int getSum(int n1,int n2);
 }
@@ -6,4 +7,14 @@ public int getSum(int n1,int n2){
 return n1+n2;
 }
 
+}
+class Main4{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int n1=sc.nextInt();
+int n2=sc.nextInt();
+CalciImpl c=new ClaciImpl();
+   int n3=c.getSum();
+System.out.println(n3);
+}
 }
