ed32a950b00af3e3122d50a75b094cf992a7ee3f
Dodda-Narasamma <156180685+Dodda-Narasamma@users.noreply.github.com>
2024-02-29 17:07:40 +0530

    Update Calci.java

--- Calci.java
+++ Calci.java
@@ -2,7 +2,7 @@ import java.util.*;
 interface Calci{
 public abstract int getSum(int n1,int n2);
 }
-class ClaiciImpl implements Calci{
+class CalciImpl implements Calci{
 public int getSum(int n1,int n2){
 return n1+n2;
 }
@@ -11,10 +11,12 @@ return n1+n2;
 class Main4{
 public static void main(String[] args){
 Scanner sc=new Scanner(System.in);
+   System.out.println("enter a num1");
 int n1=sc.nextInt();
+    System.out.println("enter a num2");
 int n2=sc.nextInt();
 CalciImpl c=new ClaciImpl();
-   int n3=c.getSum();
+   int n3=c.getSum(n1,n2);
 System.out.println(n3);
 }
 }
